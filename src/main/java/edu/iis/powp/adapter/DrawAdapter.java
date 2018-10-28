2948b2587196c41d75edc4e093e6cafa49b3f7c1
206081 <206081@10.0.75.1>
2018-10-28 17:29:42 +0100

    Lab3 3.2.1

--- src/main/java/edu/iis/powp/adapter/DrawAdapter.java
+++ src/main/java/edu/iis/powp/adapter/DrawAdapter.java
@@ -1,6 +1,7 @@
 package edu.iis.powp.adapter;
 
 import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.features.DrawerFeature;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
@@ -8,10 +9,11 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements IPlotter {
+public class DrawAdapter implements IPlotter {
 	private int startX = 0, startY = 0;
+	DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
 
-	public MyAdapter() {
+	public DrawAdapter() {
 		super();
 	}
 
@@ -26,12 +28,12 @@ public class MyAdapter extends DrawPanelController implements IPlotter {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
-		drawLine(line);
+		setPosition(x,y);
+		drawPanelController.drawLine(line);
 	}
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "Ready to Draw!";
 	}
 }
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.DrawAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.events.predefine.SelectChangeVisibleOptionListener;
 import edu.iis.powp.events.predefine.SelectTestFigureOptionListener;
@@ -42,7 +42,7 @@ public class TestPlotSoftPatterns {
 		application.addDriver("Client Plotter", clientPlotter);
 		application.getDriverManager().setCurrentPlotter(clientPlotter);
 
-		IPlotter plotter = new MyAdapter();
+		IPlotter plotter = new DrawAdapter();
 		application.addDriver("Buggy Simulator", plotter);
 
 		application.updateDriverInfo();
