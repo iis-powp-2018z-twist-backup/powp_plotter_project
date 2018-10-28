a64bfc54a26b3b80b9033bd90cd6faba53b09e92
90rl <40734302+90rl@users.noreply.github.com>
2018-10-28 16:10:00 +0100

    00_Commit test

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -6,7 +6,7 @@ import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
 
 /**
- * Plotter adapter to drawer with several bugs.
+ * Plotter adapter to drawer with several bugs.///
  */
 public class MyAdapter extends DrawPanelController implements IPlotter {
 	private int startX = 0, startY = 0;
