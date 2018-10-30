50cf7821ecbf6e747f4f91ddf75ec96c0be4d053
yach9 <dominika.jach9@gmail.com>
2018-10-30 23:31:44 +0100

    3.2.1 fixed plotter

--- src/test/java/edu/iis/powp/iplotter/IPlotterTest.java
+++ src/test/java/edu/iis/powp/iplotter/IPlotterTest.java
@@ -1,33 +1,34 @@
 package edu.iis.powp.iplotter;
-
-import edu.iis.client.plottermagic.IPlotter;
+ import edu.iis.client.plottermagic.IPlotter;
 import edu.iis.client.plottermagic.preset.FiguresJoe;
-
-/**
+ /**
  * Plotter test.
  * 
  * @author Dominik
  */
-public class IPlotterTest {
-	private static IPlotter plotter = new TestPlotter();
-
-	/**
-	 * Plotter test.
-	 */
-	public static void main(String[] args) {
-		FiguresJoe.figureScript1(plotter);
-	}
-
-	private static class TestPlotter implements IPlotter {
-
-		@Override
-		public void drawTo(int x, int y) {
-			System.out.println("Plotter drawTo action...");
-		}
-
-		@Override
-		public void setPosition(int x, int y) {
-			System.out.println("Plotter setPosition action...");
-		}
-	};
+public class IPlotterTest
+{
+    private static IPlotter plotter = new TestPlotter();
+    
+    /**
+     * Plotter test.
+     */
+    public static void main(String[] args)
+    {
+        FiguresJoe.figureScript1(plotter);
+    }
+    
+    private static class TestPlotter implements IPlotter
+    {
+         @Override
+        public void drawTo(int x, int y)
+        {
+            System.out.println("Plotter drawTo action..." + x +"  " +y); 
+        }
+         @Override
+        public void setPosition(int x, int y)
+        {
+            System.out.println("Plotter setPosition action..."+ x + "  " + y); 
+        }
+    }
 }
