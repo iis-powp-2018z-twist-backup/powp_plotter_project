e9a5028d4a97fe25cecd844ed4acba1c754c044d
LoganOn <logan15.krawczyk@gmail.com>
2018-11-04 17:15:39 +0100

    ver 1.0

--- src/test/java/edu/iis/powp/drawer/TestDrawer.java
+++ src/test/java/edu/iis/powp/drawer/TestDrawer.java
@@ -1,5 +1,6 @@
 package edu.iis.powp.drawer;
 
+import edu.iis.powp.adapter.MyAdapter;
 import edu.kis.powp.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
@@ -15,11 +16,13 @@ public class TestDrawer {
 	 * Drawer test.
 	 */
 	public static void main(String[] args) {
+		MyAdapter test = new MyAdapter();
 		DrawPanelController controller = new DrawPanelController();
 		DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(-100, -60);
 		line.setEndCoordinates(60, 130);
 		controller.drawLine(line);
+
 	}
 }
