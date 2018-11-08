668b9a8e1114030bb6d5ef32fcde4aabf0e2f4a4
Kamila <36917682+kamilasyp@users.noreply.github.com>
2018-11-08 12:52:02 +0100

    LAB 3
    3.3.1.

---
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -0,0 +1,36 @@
+package edu.iis.powp.adapter;
+
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.ILine;
+import edu.kis.powp.drawer.shape.LineFactory;
+
+public class LinePlotterAdapter {
+	private int startX = 0, startY = 0;
+	private DrawPanelController drawPanelController;
+	
+	public LinePlotterAdapter(DrawPanelController drawPanelController) {
+		super(); 
+		this.drawPanelController=drawPanelController;
+		
+	}
+
+	public void setPosition(int x, int y) {
+		this.startX = x;
+		this.startY = y;
+	}
+
+	public void drawTo(int x, int y) {
+		ILine line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.startX, this.startY);
+		line.setEndCoordinates(x, y);
+
+		drawPanelController.drawLine(line);
+		this.setPosition(x, y);
+	}
+
+	@Override
+	public String toString() {
+		return "Drawing";
+	}
+
+}
