3c5fac4eac15d7e8340bf5357eac472d66081218
Michal Sikora <206081@edu.p.lodz.pl>
2018-11-18 20:08:23 +0100

    Command 3.1.1

---
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -0,0 +1,19 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandSetPosition implements PlotterCommand {
+	private int x;
+	private int y;
+
+	public CommandSetPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public void execute(IPlotter iPlotter) {
+		iPlotter.setPosition(x, y);
+	}
+
+}
