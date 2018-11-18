7ac92e98be530ed08034c7b14156d5436b60d161
Marta Szmit <marta.szmit91@gmail.com>
2018-11-18 16:19:53 +0100

    3.2.1

--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -1,6 +1,7 @@
 package edu.iis.powp.adapter;
 
 import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.features.DrawerFeature;
 import edu.kis.powp.drawer.panel.DrawPanelController;
 import edu.kis.powp.drawer.shape.ILine;
 import edu.kis.powp.drawer.shape.LineFactory;
@@ -9,6 +10,8 @@ import edu.kis.powp.drawer.shape.LineFactory;
  * Plotter adapter to drawer with several bugs.
  */
 public class MyAdapter extends DrawPanelController implements IPlotter {
+	
+	DrawPanelController drawerController = DrawerFeature.getDrawerController();
 	private int startX = 0, startY = 0;
 
 	public MyAdapter() {
@@ -27,7 +30,8 @@ public class MyAdapter extends DrawPanelController implements IPlotter {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawerController.drawLine(line);
+		this.setPosition(x, y);
 	}
 
 	@Override
