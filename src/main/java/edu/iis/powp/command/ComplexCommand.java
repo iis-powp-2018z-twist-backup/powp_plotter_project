5e232391e7a5a349f33b7fbdf9341001f0b52748
yach9 <dominika.jach9@gmail.com>
2018-11-20 23:13:59 +0100

    3.1.2 Implements CommandDrawLineToPosition,CommandSetPosition,ComplexCommand

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -1,9 +1,31 @@
-package edu.iis.powp.command;
+ package edu.iis.powp.command;
  import edu.iis.client.plottermagic.IPlotter;
+ import java.util.List;
+ import java.util.LinkedList;
  public class ComplexCommand implements PlotterCommand {
+	 private List<PlotterCommand> commands;
+		
+		public ComplexCommand()
+		{
+			commands = new LinkedList<PlotterCommand>();
+		}
+		
+		public ComplexCommand(List<PlotterCommand> list)
+		{
+			commands = list;
+		}
+		
+		public void addCommand(PlotterCommand command)
+		{
+			commands.add(command);
+		}
+	 
  	@Override
 	public void execute(IPlotter driver) {
-		
+ 		for(PlotterCommand command : commands)
+		{
+			command.execute(driver);
+		}
 		
 	}
  }
