05373332db24895524e8da932b090190babcc7b4
Kamila <36917682+kamilasyp@users.noreply.github.com>
2018-12-30 14:19:10 +0100

    4 correction
    remove getters and setters

--- src/main/java/command/CommandSet.java
+++ src/main/java/command/CommandSet.java
@@ -6,28 +6,11 @@ public class CommandSet implements PlotterCommand {
 	private int x, y;
 
 	public CommandSet(int x, int y) {
-		this.setX(x);
-		this.setY(y);
+		this.x = x;
+		this.y = y;
 	}
 	public void execute(IPlotter driver) {
 					
-	 		driver.drawTo(getX(), getY());
+	 		driver.drawTo(x, y);
 		}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
 }
