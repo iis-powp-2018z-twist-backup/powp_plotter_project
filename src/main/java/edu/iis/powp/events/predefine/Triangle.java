c76c97dcb4685a642082224394006196c480c0a8
Kamila <36917682+kamilasyp@users.noreply.github.com>
2018-12-30 14:13:28 +0100

    3 correction
    parameter correction

--- src/main/java/edu/iis/powp/events/predefine/Triangle.java
+++ src/main/java/edu/iis/powp/events/predefine/Triangle.java
@@ -11,9 +11,9 @@ public class Triangle {
 		ComplexCommand complexCommand = new ComplexCommand();
 		
 		complexCommand.addCommand(new CommandSet(x, y));
-		complexCommand.addCommand(new CommandDrawLine(x, y));
+		
 		complexCommand.addCommand(new CommandDrawLine(x1, y1));
-		complexCommand.addCommand(new CommandDrawLine(x2, y1));
+		complexCommand.addCommand(new CommandDrawLine(x2, y2));
 		complexCommand.addCommand(new CommandDrawLine(x, y));
 				
 		return complexCommand;
