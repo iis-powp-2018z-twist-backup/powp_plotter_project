ee5beb462aff29741eaf8dd431260c3592f1591b
Jakub Wiśniewski <jakubwisn@gmail.com>
2019-02-08 03:04:15 +0100

    command 2

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,19 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+import java.util.List;
+
+public class ComplexCommand implements IPlotterCommand{
+    private List<IPlotterCommand> plotterCommands;
+
+    public ComplexCommand(List<IPlotterCommand> plotterCommands) {
+        this.plotterCommands = plotterCommands;
+    }
+
+    @Override
+    public void execute(IPlotter plotter) {
+        for (IPlotterCommand plotterCommand : plotterCommands) {
+            plotterCommand.execute(plotter);
+        }
+    }
+}
