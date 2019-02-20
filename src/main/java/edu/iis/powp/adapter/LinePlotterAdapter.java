70d35ea154d791eceff9578ba16c0d0a18bd1f28
Przemysław Cieślarek <madflow12@gmail.com>
2019-02-20 19:05:20 +0100

    L3 3.3.1

---
+++ src/main/java/edu/iis/powp/adapter/LinePlotterAdapter.java
@@ -0,0 +1,37 @@
+package edu.iis.powp.adapter;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.kis.powp.drawer.panel.DrawPanelController;
+import edu.kis.powp.drawer.shape.ILine;
+import edu.kis.powp.drawer.shape.LineFactory;
+
+public class LinePlotterAdapter implements IPlotter {
+
+    private DrawPanelController drawPanelController;
+    private ILine line;
+
+    public LinePlotterAdapter(DrawPanelController drawPanelController, ILine line) {
+        super();
+        this.drawPanelController = drawPanelController;
+        this.line = line;
+    }
+
+    private int startX = 0, startY = 0;
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void drawTo(int x, int y) {
+        ILine line = LineFactory.getSpecialLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+
+        drawPanelController.drawLine(line);
+        this.setPosition(x, y);
+    }
+
+}
--- src/main/java/edu/iis/powp/adapter/MyAdapter.java
+++ src/main/java/edu/iis/powp/adapter/MyAdapter.java
@@ -32,6 +32,7 @@ public class MyAdapter extends DrawPanelController implements IPlotter {
 		line.setEndCoordinates(x, y);
 
 		drawerPanelController.drawLine(line);
+		this.setPosition(x, y);
 	}
 
 	@Override
